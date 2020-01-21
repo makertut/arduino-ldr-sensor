16a8ef38a7d3cbf9e5487a457f76062c40ce54a3
jumejume1 <jumejume1@hotmail.com>
2020-01-21 21:53:03 +0700

    Create code1.ino

---
+++ code1.ino
@@ -0,0 +1,69 @@
+const int ledPin = 13;
+
+
+
+const int ldrPin = A0;
+
+
+
+void setup() {
+
+
+
+Serial.begin(9600);
+
+
+
+pinMode(ledPin, OUTPUT);
+
+
+
+pinMode(ldrPin, INPUT);
+
+
+
+}
+
+
+
+void loop() {
+
+
+
+int ldrStatus = analogRead(ldrPin);
+
+
+
+if (ldrStatus <=800) { 
+
+
+
+digitalWrite(ledPin, HIGH);
+
+
+
+Serial.println("LDR is DARK, LED is ON");
+
+
+
+}
+
+
+
+else {
+
+
+
+digitalWrite(ledPin, LOW);
+
+
+
+Serial.println("LED OFF");
+
+
+
+}
+
+
+
+}
